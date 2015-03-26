2b10e59db5594270fc7105218a10425584e1bcd1
Jeff Sember <jpsember@gmail.com>
2015-03-26 13:01:44 -0700

    Issue #20: enhancing feature point algorithm a bit; setting feature point in center of 'clumped' points

--- Gest/src/com/js/gest/FeaturePointList.java
+++ Gest/src/com/js/gest/FeaturePointList.java
@@ -9,6 +9,9 @@ import com.js.basic.Point;
 
 class FeaturePointList {
 
+  private static final float FEATURE_LENGTH_MIN = StrokeSet.STANDARD_WIDTH * .05f;
+  private static final float FEATURE_ANGLE_MIN = MyMath.M_DEG * 80.0f;
+
   /**
    * Construct a FeaturePointList for a stroke
    */
@@ -34,29 +37,31 @@ class FeaturePointList {
 
   private void determineFeaturePoints(Stroke s) {
 
-    final float FEATURE_LENGTH_MIN = StrokeSet.STANDARD_WIDTH * .05f;
-    final float FEATURE_ANGLE_MIN = MyMath.M_DEG * 70.0f;
-
     mBitmap = new BitSet(s.size());
 
     float previousAngle = 0;
     boolean previousAngleDefined = false;
+
+    int clumpedPointsCount = 0;
     for (int i = 1; i < s.size(); i++) {
       Point aPt = s.getPoint(i - 1);
       Point bPt = s.getPoint(i);
 
       float distance = MyMath.distanceBetween(aPt, bPt);
-      if (distance < FEATURE_LENGTH_MIN)
+      if (distance < FEATURE_LENGTH_MIN) {
+        clumpedPointsCount++;
         continue;
+      }
       float angle = MyMath.polarAngleOfSegment(aPt, bPt);
       if (previousAngleDefined) {
         float angleDiff = Math
             .abs(MyMath.normalizeAngle(angle - previousAngle));
         if (angleDiff >= FEATURE_ANGLE_MIN)
-          mBitmap.set(i - 1);
+          mBitmap.set(i - 1 - clumpedPointsCount/2);
       }
       previousAngle = angle;
       previousAngleDefined = true;
+      clumpedPointsCount = 0;
     }
   }
 
